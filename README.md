aec2923803d97e875c5892b3da36aaed04417cd0
Masahiro Kanai <masahiro.kanai@riken.jp>
2018-02-10 07:27:05 -0500

    first commit: v1.0.0

---
+++ README.md
@@ -0,0 +1,86 @@
+# <img src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f5fb.png" width="42px"> Fuji plot <img src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f5fb.png" width="42px">
+a circos representation of multiple GWAS results.
+
+## Overview
+Fuji plot (a circos representation of multiple GWAS results) was developed to efficiently summarize significant SNPs across dozens of traits ([Kanai, M. *et al*., *Nat. Genet.* 2018](http://dx.doi.org/10.1038/s41588-018-0047-6)). Technically, it is a wrapper script for [Circos](http://circos.ca/), generating input data and configuration files from GWAS results.
+
+<p align="center"><img src="http://mkanai.github.io/img/Kanai2018_Fig1.svg" width="640px"></p>
+
+Fuji plot is named after <img src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f5fb.png" width="20px"> Mt. Fuji in Japan <img src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f5fb.png" width="20px">, whose ridge and skirts greatly resemble our figure!
+
+<p align="center"><img src="https://i.imgur.com/QXlnKZk.jpg" width="480px"></p>
+
+## Requirements
+* [R](https://www.r-project.org/) with [dplyr](https://github.com/tidyverse/dplyr) and [stringr](https://github.com/tidyverse/stringr) packages.
+  * To install these packages, type `install.packages("tidyverse")` in R console.
+* [Circos](http://circos.ca/)
+  * Please follow [their instructions](http://circos.ca/documentation/tutorials/configuration/installation/) to install Circos.
+  * For the current version ([v0.69-6](http://circos.ca/distribution/circos-0.69-6.tgz)), you need to apply our [patch](https://gist.github.com/mkanai/be05f40f933112bfb70bb08076cdaa00) to avoid an infinite loop in text arrangements. Alternatively, you can download and use [our patched version](https://www.dropbox.com/s/z6jdwhj0o570fp8/circos-0.69-6-kanai.tgz?dl=0). I will report this issue soon.
+
+## Usage
+Please add `circos` to your `PATH` or set `CIRCOS_PATH` in `fujiplot.R` appropriately.
+
+```{sh}
+Rscript fujiplot.R input_example/input.txt input_example/traitlist.txt
+```
+
+## Inputs
+
+Input files used in [Kanai, M. *et al.* (2018)](http://dx.doi.org/10.1038/s41588-018-0047-6) are provided under `./input_example`.
+
+### Significant trait-associated loci (`input.txt`)
+
+This file provides a concatenated list of all significant *trait-associated* loci (for each trait). It also defines a unique locus across multiple traits to highlight pleiotropic loci. The required fields are as follows:
+
+* `LOCUS_ID`: Unique ID for each locus
+* `CATEGORY`: Trait category
+* `TRAIT`: Trait name
+* `CHR`: Chromosome
+* `BP`: Position (hg19)
+* `MARKER`: Marker ID
+* `GENE`: Gene symbol
+
+
+### Trait list (`traitlist.txt`)
+
+This file provides a list of traits and their categories. It defines an order of traits and a color of each category in a figure. The required fields are as follows:
+
+* `CATEGORY`: Trait category
+* `TRAIT`: Trait name
+* `COLOR`: Category color
+
+## Outputs
+
+All output files will be created under `./output`.
+
+
+### Circos outputs (`circos.{png,svg}`)
+This is the raw output from Circos. Unfortunately, we could not figure out the appropriate way to make our [highlights](http://circos.ca/documentation/tutorials/highlights/) (radial lines) over [scatter plots](http://circos.ca/documentation/tutorials/2d_tracks/scatter_plots/). To this end, we edited the svg file using Adobe Illustrator and produced the final figure.
+
+<p align="center">![fujiplot_output](./output/circos.svg)</p>
+
+
+### Optional: a bar plot (`barplot.pdf`)
+
+This corresponds to Fig. 1a of [Kanai, M. *et al.* (2018)](http://dx.doi.org/10.1038/s41588-018-0047-6). Again, you need to do some Illustrator edits to make it pretty. You can suppress this output by setting `OUTPUT_BARPLOT = FALSE` in `fujiplot.R`.
+
+<p align="center">![fujiplot_output](./output/barplot.pdf)</p>
+
+
+## Citation
+When using Fuji plot, please cite the following paper.
+
+* Kanai, M., *et al*. Genetic analysis of quantitative traits in the Japanese population links cell types to complex human diseases. *Nat. Genet.* (2018) [doi:10.1038/s41588-018-0047-6](http://dx.doi.org/10.1038/s41588-018-0047-6)
+
+## Contact
+Masahiro Kanai (mkanai@g.harvard.edu)
+
+http://mkanai.github.io/
+
+## Links
+* [JENGER](http://jenger.riken.jp/en/) (the lab website)
+* [The BioBank Japan Project](https://biobankjp.org/english/index.html)
+* [RIKEN Center for Integrative Medical Sciences](http://www.ims.riken.jp/english/)
+* [National Bioscience Database Center Human Database](https://humandbs.biosciencedbc.jp/en/)
+* [Circos](http://circos.ca/)
+* [ClicO FS](http://codoncloud.com:3000/)
---
+++ fujiplot.R
@@ -0,0 +1,184 @@
+suppressMessages(library(dplyr))
+library(stringr)
+
+CIRCOS_PATH = "circos"
+CIRCOS_DEBUG_GROUP = "text,textplace"
+OUTPUT_BARPLOT = TRUE
+
+# constants
+.VERSION = "1.0.0"
+MY_COLOR_CONF = "./config/my_color.conf"
+MY_SCATTER_BACKGROUND_CONF = "./config/my_scatter_background.conf"
+HIGHLIGHT_DATA = "./data_tracks/highlights.txt"
+SCATTER_DATA = "./data_tracks/scatter.txt"
+STACKED_DATA = "./data_tracks/stacked.txt"
+LABEL_DATA = "./data_tracks/label.txt"
+SCATTER_BACKGROUND_COLOR_ALPHA = 0.3
+LARGE_POINT_SIZE = 16
+SMALL_POINT_SIZE = 8
+
+################################################################################
+writeLines(c("*********************************************************************",
+             "* Fuji plot -- a circos representation of multiple GWAS results",
+     sprintf("* Version %s", .VERSION),
+             "* Masahiro Kanai (mkanai@g.harvard.edu)",
+             "* Harvard Medical School / RIKEN IMS / Osaka Univerisity",
+             "* GNU General Public License v3",
+             "*********************************************************************"
+           ))
+
+args = commandArgs(trailingOnly = T)
+if (identical(args, character(0))) {
+  args = str_c("./input_example/", c("input.txt", "traitlist.txt"))
+}
+input_fname = args[1]
+traitlist_fname = args[2]
+
+
+################################################################################
+# load data
+message("Loading input files...")
+
+df = read.table(input_fname, T, sep = "\t")
+traitlist = read.table(traitlist_fname, T, sep = '\t', as.is = T, quote = '', comment.char = '', fileEncoding='utf-8')
+
+n_loci = length(unique(df$LOCUS_ID))
+writeLines(c(
+     sprintf("* Input data: %s", input_fname),
+     sprintf("* Number of significant SNPs: %d", nrow(df)),
+     sprintf("* Number of unique loci: %d", n_loci),
+             "",
+     sprintf("* Trait list: %s", traitlist_fname),
+     sprintf("* Number of traits: %d (%s)", nrow(traitlist), str_c(traitlist$TRAIT, collapse = ',')),
+     sprintf("* Number of categories: %d (%s)", length(unique(traitlist$CATEGORY)), str_c(unique(traitlist$CATEGORY), collapse = ','))
+           ))
+
+input_traits = traitlist$TRAIT
+traitlist = traitlist %>% filter(TRAIT %in% df$TRAIT) %>%
+                           mutate(idx = 1:n(),
+                                  category_lower = str_to_lower(str_replace(CATEGORY, ' ', '_'))) %>%
+                           mutate(parameters = str_c('fill_color=', category_lower))
+excluded_traits = setdiff(input_traits, traitlist$TRAIT)
+writeLines(c("",
+     sprintf("Excluded %d traits because of no significant SNPs (%s).", length(excluded_traits), str_c(excluded_traits, collapse = ',')),
+             ""
+           ))
+
+################################################################################
+message("Generating configuration and data files for circos...")
+
+# output color config
+str_c_comma = function(x){str_c(x, collapse = ",")}
+cols = traitlist %>% select(category_lower, COLOR) %>%
+                     unique() %>%
+                     mutate(rgb = apply(t(col2rgb(COLOR)), 1, str_c_comma),
+                            rgba = apply(floor(t((1 - SCATTER_BACKGROUND_COLOR_ALPHA) * 255 + SCATTER_BACKGROUND_COLOR_ALPHA * col2rgb(COLOR))), 1, str_c_comma))
+writeLines(c("<colors>",
+             sprintf("\t%s = %s\n\talpha_%s = %s", cols$category_lower, cols$rgb, cols$category_lower, cols$rgba),
+             "</colors>"), MY_COLOR_CONF)
+message(sprintf("* Color configuration: %s", MY_COLOR_CONF))
+
+
+################################################################################
+# output scatterplot background config
+colsep = table(factor(traitlist$category_lower, levels=unique(traitlist$category_lower)))
+bg = data.frame(category_lower = unique(traitlist$category_lower),
+                y0 = nrow(traitlist) - cumsum(colsep) - 0.5,
+                y1 = nrow(traitlist) - cumsum(c(0,colsep))[1:length(colsep)] - 0.5)
+writeLines(c("<backgrounds>",
+             sprintf("<background>\n\tcolor = alpha_%s\n\ty0 = %.1f\n\ty1 = %.1f\n</background>", bg$category_lower, bg$y0, bg$y1),
+             "</backgrounds>"), MY_SCATTER_BACKGROUND_CONF)
+message(sprintf("* Scatter background configuration: %s", MY_SCATTER_BACKGROUND_CONF))
+
+
+################################################################################
+# output pleiotropy highlight data
+nsnps_per_locus = df %>% group_by(LOCUS_ID) %>% summarize(n = n())
+df = df %>% mutate(CHR = str_c("hs", CHR),
+                   nsnps = nsnps_per_locus$n[match(LOCUS_ID, nsnps_per_locus$LOCUS_ID)])
+
+most_common = function(x) {tail(names(sort(table(x))), 1)}
+inter_categorical = df %>% group_by(LOCUS_ID) %>% summarize(CHR = most_common(CHR),
+                                                            BP = most_common(BP),
+                                                            n = length(unique(CATEGORY))) %>% 
+                                                  filter(n > 1)
+write.table(inter_categorical[c("CHR", "BP", "BP")], HIGHLIGHT_DATA, sep = "\t", row.names = F, col.names = F, quote = F)
+message(sprintf("* Highlights data (inter-categorical pleiotropic loci): %s", HIGHLIGHT_DATA))
+
+
+################################################################################
+# output outer scatter plot data
+scatter = merge(df, traitlist, by = "TRAIT", all.x = T)
+scatter$value = nrow(traitlist) - scatter$idx
+scatter$parameters = str_c(scatter$parameters, str_c('z=', scatter$nsnps), str_c('glyph_size=', ifelse(scatter$nsnps > 1, LARGE_POINT_SIZE, SMALL_POINT_SIZE)), sep = ",")
+scatter = scatter[order(scatter$nsnps, decreasing=T),]
+write.table(scatter[c("CHR", "BP", "BP", "value", "parameters")], SCATTER_DATA, sep = "\t", row.names = F, col.names = F, quote = F)
+message(sprintf("* Scatter plot data (significant loci): %s", SCATTER_DATA))
+
+
+################################################################################
+# output inner stacked scatter plot data
+stacked = list()
+stacked_y = rep(0, n_loci)
+names(stacked_y) = 1:n_loci
+
+for (i in 1:nrow(traitlist)) {
+  x = subset(scatter, idx == i)
+  x$value = stacked_y[x$LOCUS_ID]
+  stacked_y[x$LOCUS_ID] = stacked_y[x$LOCUS_ID] + 1
+  stacked[[i]] = x
+}
+
+stacked = do.call(rbind, stacked)
+stacked$parameters = str_c(stacked$parameters, str_c('z=', stacked$nsnps), sep = ",")
+stacked = stacked[order(stacked$nsnps, decreasing=T),]
+write.table(stacked[c("CHR", "BP", "BP", "value", "parameters")], STACKED_DATA, sep = "\t", row.names = F, col.names = F, quote = F)
+message(sprintf("* Stacked bar plot data (# significant SNPs per locus): %s", STACKED_DATA))
+
+
+################################################################################
+# output label data
+label = df %>% filter(LOCUS_ID %in% inter_categorical$LOCUS_ID) %>%
+                 group_by(LOCUS_ID) %>%
+                   summarize(CHR = most_common(CHR),
+                             BP = most_common(BP),
+                             GENE = most_common(GENE))
+write.table(label[c("CHR", "BP", "BP", "GENE")], LABEL_DATA, sep = "\t", row.names = F, col.names = F, quote = F)
+message(sprintf("* Label data (name of inter-categorical pleiotropic loci): %s", LABEL_DATA))
+
+
+################################################################################
+# call circos
+cmd = sprintf("%s -conf ./config/circos.conf -debug_group %s", CIRCOS_PATH, CIRCOS_DEBUG_GROUP)
+writeLines(c("",
+             "Calling circos to plot...",
+     sprintf("* Call: %s", cmd),
+             ""
+           ))
+system(cmd)
+
+
+################################################################################
+# output bar plot
+if (OUTPUT_BARPLOT) {
+  bar = df %>% group_by(TRAIT) %>%
+               summarize(total = length(MARKER),
+                         pleiotropic = length(MARKER[nsnps > 1]),
+                         inter_categorical = length(MARKER[LOCUS_ID %in% inter_categorical$LOCUS_ID])) %>%
+               mutate(single = total - pleiotropic,
+                      intra_categorical = pleiotropic - inter_categorical)
+  bar = merge(bar, traitlist, by = "TRAIT")
+  bar = bar[order(bar$idx, decreasing=T),]
+  rownames(bar) = bar$TRAIT
+
+  cairo_pdf("./output/barplot.pdf", width = 8, height = 8, family = "Helvetica")
+    barplot(t(bar[,c("inter_categorical", "intra_categorical", "single")]), ylim = c(0, 100), space = 0, col = c("black", "grey50", "white"))
+  . = dev.off()
+}
+
+
+writeLines(c("", "",
+             "* Final circos outputs: ./output/circos.{png,svg}.",
+             "",
+     sprintf("Finished at %s.", Sys.time())
+           ))
